3d9d538516432fbd82ba6f510ecfdab768c42b39
Amee <ameecova@yahoo.com>
2024-06-07 15:15:00 +0200

    Create doc notes, write notes for next steps in reading the data with pandas

--- log_analyzer/cli.py
+++ log_analyzer/cli.py
@@ -8,7 +8,26 @@ click.command()
 @click.option('--eps', is_flag=True, help='Events per second')
 @click.option('--bytes', is_flag=True, help='Total amount of bytes exchanged')
 def analyze(inputs, output, mfip, lfip, eps, bytes):
-    pass
+    """
+    Analyze log files and perform the following operations in options
+
+    Arguments:
+        input: Path to one or more input files.
+        output: Path to a file to save output in plain text JSON format.
+    Options:
+        --mfip: most frequent IP
+        --lfip: least frequent IP
+        --eps: events per second
+        --bytes: total amount of bytes exchanged
+    """
+
+    try:
+        pass
+        # Read the data into a dataframe with pandas?
+    
+    #Exceptions that we should catch
+    except:
+        pass
 
 if __name__ == '__main__':
     analyze()
